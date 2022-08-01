83c2f932e905c5932e202cc29e9fcc9c67084368
Raul <raulrufino0@gmail.com>
2022-08-01 07:51:28 -0300

    Add init module file

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,11 @@
 
 import logging 
+from src.settings import LOG_FILE, LOG_LEVEL
 
-logging.basicConfig(filename='engine.log')
+logging.basicConfig(filename=LOG_FILE)
 
 logger = logging.getLogger('engine')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(LOG_LEVEL)
 
 def send_uci_msg(msg):
     logger.info(f'sending UCI message: {msg}')
---
+++ src/settings.py
@@ -0,0 +1,5 @@
+
+import logging
+
+LOG_FILE = 'engine.log'
+LOG_LEVEL = logging.DEBUG
