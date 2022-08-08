c79a40ab100939fa698c9114dc9560b41ca36b9b
Raul <raulrufino0@gmail.com>
2022-08-07 22:21:52 -0300

    Remove duplicated logic when generating a sequence of movements

--- src/movements/rook.py
+++ src/movements/rook.py
@@ -13,49 +13,30 @@ def is_in_first_column(index: int) -> bool:
 def is_in_last_column(index: int) -> bool:
     return (index + 1) % 8 == 0
 
-def generate_forward_rook_moves(squares, rook_index: int, adversary_pieces) -> list[Move]:
+def generate_range_of_moves(squares, piece_index: int, adversary_pieces, check_boundary, get_next_index) -> list[Move]:
     moves = []
-    next_index = rook_index + 8
+    if check_boundary(piece_index):
+        return []
+    next_index = get_next_index(piece_index)
     while squares[next_index] == EmptySquare or squares[next_index] in adversary_pieces:
-        moves.append([rook_index, next_index])
-        if is_in_last_line(next_index) or squares[next_index] in adversary_pieces: 
+        moves.append([piece_index, next_index])
+        if check_boundary(next_index) or squares[next_index] in adversary_pieces: 
             break
         else:
-            next_index += 8
+            next_index = get_next_index(next_index)
     return moves
+
+def generate_forward_rook_moves(squares, rook_index: int, adversary_pieces) -> list[Move]:
+    return generate_range_of_moves(squares, rook_index, adversary_pieces, is_in_last_line, lambda x: x + 8)
 
 def generate_backward_rook_moves(squares, rook_index: int, adversary_pieces) -> list[Move]:
-    moves = []
-    next_index = rook_index - 8
-    while squares[next_index] == EmptySquare or squares[next_index] in adversary_pieces:
-        moves.append([rook_index, next_index])
-        if is_in_first_line(next_index) or squares[next_index] in adversary_pieces:
-            break
-        else:
-            next_index -= 8
-    return moves
+    return generate_range_of_moves(squares, rook_index, adversary_pieces, is_in_first_line, lambda x: x - 8)
 
 def generate_left_rook_moves(squares, rook_index: int, adversary_pieces) -> list[Move]:
-    moves = []
-    next_index = rook_index - 1
-    while squares[next_index] == EmptySquare or squares[next_index] in adversary_pieces:
-        moves.append([rook_index, next_index])
-        if is_in_first_column(next_index) or squares[next_index] in adversary_pieces:
-            break
-        else:
-            next_index -= 1
-    return moves
+    return generate_range_of_moves(squares, rook_index, adversary_pieces, is_in_first_column, lambda x: x - 1)
 
 def generate_right_rook_moves(squares, rook_index: int, adversary_pieces) -> list[Move]:
-    moves = []
-    next_index = rook_index + 1
-    while squares[next_index] == EmptySquare or squares[next_index] in adversary_pieces:
-        moves.append([rook_index, next_index])
-        if is_in_last_column(next_index) or squares[next_index] in adversary_pieces:
-            break
-        else:
-            next_index += 1
-    return moves
+    return generate_range_of_moves(squares, rook_index, adversary_pieces, is_in_last_column, lambda x: x + 1)
 
 def get_all_rook_moves(squares, rook_index: int, current_player: Player) -> list[Move]:
     moves = []
@@ -63,13 +44,9 @@ def get_all_rook_moves(squares, rook_index: int, current_player: Player) -> list
         adversary_pieces = BlackPieces
     else:
         adversary_pieces = WhitePieces
-    if not is_in_last_line(rook_index):
-        moves.extend(generate_forward_rook_moves(squares, rook_index, adversary_pieces))
-    if not is_in_first_line(rook_index):
-        moves.extend(generate_backward_rook_moves(squares, rook_index, adversary_pieces))
-    if not is_in_last_column(rook_index):
-        moves.extend(generate_right_rook_moves(squares, rook_index, adversary_pieces))
-    if not is_in_first_column(rook_index):
-        moves.extend(generate_left_rook_moves(squares, rook_index, adversary_pieces))
+    moves.extend(generate_forward_rook_moves(squares, rook_index, adversary_pieces))
+    moves.extend(generate_backward_rook_moves(squares, rook_index, adversary_pieces))
+    moves.extend(generate_right_rook_moves(squares, rook_index, adversary_pieces))
+    moves.extend(generate_left_rook_moves(squares, rook_index, adversary_pieces))
     return moves
 
